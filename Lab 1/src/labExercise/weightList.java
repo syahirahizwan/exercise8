70f1242ff3c25080115d3d934eec55c67bd976c4
user.name syahirahizwan <user.email syahirahizwan@gmail.com>
2021-03-18 18:37:28 +0800

    second commit

---
+++ Lab 1/src/labExercise/weightList.java
@@ -0,0 +1,14 @@
+package labExercise;
+import java.util.List;
+
+public class weightList {
+	
+	public static void main(String[] args)
+	{
+		System.out.print("\nA list of humans' weight on Mars and Earth\n");
+		
+		HumanWeight human = new HumanWeight();
+		//List<demoHumanWeight> demo = human.selectDemo();
+	}
+	
+}
